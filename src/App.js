af2853fcf143f81ead0d9805803c1e6c09f0920f
Sandhya <sandhya.k@tringapps.net>
2024-04-29 18:13:24 +0530

    Removed audit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import ForgotPassword from './Components/forgetPassword';
 import ResetPassword from './Components/resetPassword';
 import Register from './Components/register';
 // import Dashboard from './Components/dashboard';
-import SendInvite from './Components/sendInvite';
+// import SendInvite from './Components/sendInvite';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login';
 import React, { useEffect } from 'react';
 // import { Helmet } from 'react-helmet';
-import UserPop from './Components/UserPop';
-import OrgPop from './Components/OrganizationPopup';
-import DataGrid from './Components/Audit';
+// import UserPop from './Components/UserPop';
+// import OrgPop from './Components/OrganizationPopup';
+// import DataGrid from './Components/Audit';
 
 function App() {
   useEffect(() => {
@@ -30,10 +30,10 @@ function App() {
       <Route path="/login" element={<Login/>} />
       <Route path="/register" element={<Register/>} />
       {/* <Route path="/dashboard" element={<Dashboard/>} /> */}
-      <Route path="/send-invite" element={<SendInvite />} />
+      {/* <Route path="/send-invite" element={<SendInvite />} />
       <Route path="/UserPop" element={<UserPop />} />
-      <Route path="/OrgPop" element={<OrgPop />} />
-      <Route path="/Audit" element={<DataGrid />} />
+      <Route path="/OrgPop" element={<OrgPop />} /> */}
+      {/* <Route path="/Audit" element={<DataGrid />} /> */}
 
     </Routes>
   </Router>
